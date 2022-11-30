e8c684fb2c8c18bb01e95e335f2570f11f22c4c9
Sam <56273381+agentsamOTU@users.noreply.github.com>
2022-11-29 21:55:36 -0500

    Altered mouse code with calibrated values

--- mouseCode/mouseCode.ino
+++ mouseCode/mouseCode.ino
@@ -15,27 +15,33 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
 int LMB_PIN = 2;
 int RMB_PIN = 3;
 
+int GRLED_PIN = 5;
+int RDLED_PIN = 6;
+
 int LMB_LVALUE= 0;
 int RMB_LVALUE= 0;
 
-int LMB_APIN = A0;
-int RMB_APIN = A1;
+int LMB_APIN = A1;
+int RMB_APIN = A0;
 int STEERING_APIN=A2;
 
-int LMB_AVALUE = 128;
-int RMB_AVALUE = 128;
+int LMB_AVALUE = 0;
+int RMB_AVALUE = 0;
 int STEERING_AVALUE = 128;
 
 void setup() {
   // Initialize Button Pins
+  Serial.begin(9600);
   pinMode(LMB_PIN, INPUT_PULLUP);
   pinMode(RMB_PIN, INPUT_PULLUP);
+  pinMode(GRLED_PIN,OUTPUT);
+  pinMode(RDLED_PIN,OUTPUT);
  
   // Initialize Joystick Library
   Joystick.begin();
-  Joystick.setAcceleratorRange(0,256);
-  Joystick.setBrakeRange(0,256);
-  Joystick.setSteeringRange(0,256);
+  Joystick.setAcceleratorRange(0,100);
+  Joystick.setBrakeRange(0,100);
+  Joystick.setSteeringRange(256,0);
 
 }
 
@@ -59,14 +65,22 @@ void loop() {
 
   //Read analog pin values
   //Need to adjust based on sensors
-  LMB_AVALUE = analogRead(LMB_APIN)/4;
-  RMB_AVALUE = analogRead(RMB_APIN)/4;
+  LMB_AVALUE = (analogRead(LMB_APIN)-520)*-3;
+  RMB_AVALUE = (analogRead(RMB_APIN)-526)*-3;
+  Serial.print("L ");
+  Serial.print(LMB_AVALUE);
+  Serial.print("\n");
+  Serial.print("R ");
+  Serial.print(RMB_AVALUE);
+  Serial.print("\n");
   STEERING_AVALUE = analogRead(STEERING_APIN)/4;
 
   Joystick.setAccelerator(LMB_AVALUE);
   Joystick.setBrake(RMB_AVALUE);
   Joystick.setSteering(STEERING_AVALUE);
+
+  analogWrite(GRLED_PIN,LMB_AVALUE*2.5);
+  analogWrite(RDLED_PIN,RMB_AVALUE*2.5);
   
   delay(10);
 }
-
